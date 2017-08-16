d9047babbd5d7f6efc43256b9292ed7f84730be6
Ezequiel Sosa <ezequieljsosa@gmail.com>
2017-08-16 15:16:43 -0300

    fix travis yml

---
+++ VARDB/Query/VariantQueryResult.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 27, 2017
+
+@author: eze
+'''
+
+class VariantQueryResult(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.samples = []
